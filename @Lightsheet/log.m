d048391f34c36206267a1fe521b0f69467c781a2
Jean Perrin <Jean Perrin@1P>
2017-06-27 11:38:26 +0200

    Fixed the 'System Volume Information' bug in the log system

--- @Lightsheet/log.m
+++ @Lightsheet/log.m
@@ -31,12 +31,13 @@ end
 
 % --- Find the log file
 slist = get(this.UI.Study, 'String');
+slist = setdiff(slist, 'System Volume Information');
 if isempty(get(this.UI.Root, 'String')) || isempty(slist)
     
     buffer = [buffer logtxt];
     
 else
-    
+        
     path = [get(this.UI.Root, 'String') filesep ...
         slist{get(this.UI.Study, 'Value')} filesep ...
         get(this.UI.Date, 'String')];
